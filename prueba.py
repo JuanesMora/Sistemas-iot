02e2f81213cce1e29c4c832efa92ed063e92d5e0
JuanesMora <jmoradi@unal.edu.com>
2026-02-09 17:53:35 -0500

    first commit

---
+++ prueba.py
@@ -0,0 +1,14 @@
+
+nombre = input("Digita tu nombre")
+n = int(input("Digite su edad"))
+
+if n >= 18:
+    print("ud es mayor de edad")
+else:
+    print("ud es menor de edad")
+
+print(f"tu nombres es: {nombre} y tu edad es {n}")
+
+
+for i in range(1,11):
+    print(i)
